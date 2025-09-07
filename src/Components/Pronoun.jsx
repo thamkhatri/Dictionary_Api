2c02aa6154dfdebaa4540ac87fde2974e2105e80
tham khatri <thamkhatri2003@gmail.com>
2025-09-07 16:23:40 +0545

    Reafctor-(css)

--- src/Components/Pronoun.jsx
+++ src/Components/Pronoun.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Pronoun = ({ data }) => {
     return (
         <div className='px-5'>
-            <div className='bg-green-50 rounded-2xl shadow-xl px-3 md:p-6  flex flex-col text-center outline-2 outline-offset-2 outline-emerald-500 mb-8 '>
+            <div className='bg-green-50 rounded-2xl shadow-xl px-3 md:p-6  flex flex-col text-center outline-2 outline-offset-2 outline-emerald-500 mb-4 md:mb-8 '>
                 <h2 className='text-lg md:text-4xl font-extrabold text-emerald-700 '>{data.word}</h2>
                 <hr />
                 <p className=' text-gray-500 text-xs md:text-lg mb-2'>{data.phonetic}</p>
